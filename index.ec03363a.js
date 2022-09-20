55b9053369146f5a39ab2ea445917233c28b4256
ViktorSolomianiy <108832355+ViktorSolomianiy@users.noreply.github.com>
2022-09-20 14:23:40 +0000

    Deploying to gh-pages from @ ViktorSolomianiy/proj-icecream-team-14@bffc91a8512d0a8be20b896f42f224b265a1b586 🚀

---
+++ index.ec03363a.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-prod-open]"),closeMenuBtn:document.querySelector("[data-prod-close]"),menu:document.querySelector("[data-prod]")};function n(){e.menu.classList.toggle("is-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.ec03363a.js.map
